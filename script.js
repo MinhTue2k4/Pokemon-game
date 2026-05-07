e8bcb134ccd7b88a96b89269d36c3e0013336c6f
MinhTue2k4 <artanis1607@gmail.com>
2026-05-07 12:01:15 +0700

    .

--- script.js
+++ script.js
@@ -36,23 +36,23 @@ const app = {
         conTainer.innerHTML = `<div class="row"> ${htmls.join('')} </div>`;
     },
     handleEvents: function () {
-        the1 = null;
-        the2 = null;
+        the1 = the2 = null;
         timerID = null;
         matchedCards = 0;
         totalPairs = this.heroes.length;
 
-        function setupCardClicks() {
+        function setupCardConditions() {
             const cards = document.getElementsByClassName("cards")
             for (let card of cards) {
                 card.addEventListener("click", function () {
                     if (this === the1 || the1 !== null && the2 !== null) {
                         return;
                     }
-                    this.classList.add('open-focus')
+                    this.classList.add('flipped-card')
                     if (the1 === null) {
                         the1 = this;
-                    } else {
+                    }
+                    else {
                         the2 = this;
                         checkCardStatus(the1, the2);
                         setTimeout(function () {
@@ -65,23 +65,20 @@ const app = {
         }
 
         function checkCardStatus(img1, img2) {
-            let heroImg1 = img1.querySelector('.hero')
-            let heroImg2 = img2.querySelector('.hero')
-            let heroThe1 = heroImg1.getAttribute('src')
-            let heroThe2 = heroImg2.getAttribute('src')
+            let heroImg1 = img1.querySelector('.hero')?.getAttribute('src')
+            let heroImg2 = img2.querySelector('.hero')?.getAttribute('src')
             // match card 
-            if (heroThe1 === heroThe2) {
-                matchedCards++;
-                setTimeout(function () {
-                    img1.style.opacity = '0';
-                    img2.style.opacity = '0';
+            if (heroImg1 === heroImg2) {
+                matchedCards++
+                setTimeout(() => {
+                    img1.classList.add('match-card')
+                    img2.classList.add('match-card')
                 }, 600)
                 //THẮNG  
                 if (matchedCards == totalPairs) {
                     clearInterval(timerID)
                     btnStart.classList.add('is-blinking');
                     btnStart.disabled = false
-
                     setTimeout(() => {
                         alert('Winner!')
                     }, 500)
@@ -89,46 +86,44 @@ const app = {
             }
             else {
                 setTimeout(function () {
-                    img1.classList.remove('open-focus');
-                    img2.classList.remove('open-focus');
+                    img1.classList.remove('flipped-card');
+                    img2.classList.remove('flipped-card');
                 }, 800);
             }
-            // console.log('matchedCards:', matchedCards)
-            // console.log('totalPairs:', totalPairs)
         }
         //
         btnStart.addEventListener("click", function () {
-            btnStart.classList.remove('is-blinking');
-            conTainer.style.opacity = '100';
+            btnStart.classList.remove('is-blinking')
             btnStart.disabled = true
-
-            app.render();
-            setupCardClicks();
+            conTainer.classList.add('is-playing');
 
             let timeLeft = 60;
             matchedCards = 0;
             clearInterval(timerID);
 
+            app.render();
+            setupCardConditions();  
+
             timerID = setInterval(() => {
                 timeLeft--;
                 countdownBar.style.width = (timeLeft / 60) * 100 + '%';
-
                 //THUA  
                 if (timeLeft <= 0) {
                     clearInterval(timerID);
-                    const allCards = document.querySelectorAll('.cards');
-                    for (let card of allCards) {
-                        card.style.pointerEvents = 'none';
-                    }
+                    const allCards = $$('.cards');
+                    allCards.forEach((card) => {
+                        card.classList.add('is-locked')
+                    })
                     btnStart.classList.add('is-blinking');
                     btnStart.disabled = false;
                     setTimeout(() => {
                         alert('Thua rồi!');
+                        conTainer.classList.add('transit-animation');
+                        conTainer.classList.remove('is-playing');
                     }, 500);
                 }
             }, 1000);
         });
-        setupCardClicks();
     },
     start: function () {
         this.render()
