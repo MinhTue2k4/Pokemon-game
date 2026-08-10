89db5f2215d5411a854cd631387ec990bd244424
MinhTue2k4 <artanis1607@gmail.com>
2026-08-10 22:55:49 +0700

    remove render() from game.ts, replace by function that map to data HeroCard[]. Adding renderCards function to UI.ts || remove ! and get  possibly 'null'bug in UI

--- game.ts
+++ game.ts
@@ -3,41 +3,35 @@ import type { HeroApi, HeroCard } from './api.js';
 export interface GameState {
     all_Heroes_Data: HeroApi[];
     heroes_Data_In_Use: HeroCard[];
-    render(...)...//hmm..???
+    createDeck(): HeroCard[];
 }
 
 export const game: GameState = {
     all_Heroes_Data: [],
     heroes_Data_In_Use: [],
-    render: function (conTainer: HTMLDivElement) {
-        // pick hero phase
-        this.all_Heroes_Data.sort(() => Math.random() - 0.5);
-        const random10Heroes = this.all_Heroes_Data.slice(0, 10);
-        this.heroes_Data_In_Use = random10Heroes.map(hero => {
+
+    createDeck: function (): HeroCard[] {
+        // pick all random heroes
+        const shuffledAllHeroes = [...this.all_Heroes_Data].sort(
+            () => Math.random() - 0.5
+        );
+
+        // pick 10 random heroes
+        const random10Heroes = shuffledAllHeroes.slice(0, 10);
+
+        this.heroes_Data_In_Use = random10Heroes.map((hero) => {
             return {
                 name: hero.localized_name,
                 image: 'https://cdn.steamstatic.com' + hero.img,
             };
         });
 
-        // x2 array of hero  
-        let double_the_Heroes = this.heroes_Data_In_Use.concat(this.heroes_Data_In_Use);
-        
-        // random -50 to 50%
-        double_the_Heroes.sort(function () {
-            return Math.random() - 0.5;
-        });
+        // x2 vitamin C  
+        const doubleTheHeroes = this.heroes_Data_In_Use.concat(this.heroes_Data_In_Use)
 
-        const htmls = double_the_Heroes.map(defineHero);
-        function defineHero(hero: HeroCard) {
-            return `
-                <div class="cards">
-                    <img src="./assets/cover.jpg" class="cover">
-                    <img src="${hero.image}" class="hero">
-                </div>
-            `;
-        }
+        // Xáo lần cuối cùng
+        doubleTheHeroes.sort(() => Math.random() - 0.5);
 
-        conTainer.innerHTML = `<div class="row"> ${htmls.join('')} </div>`;
-    }
+        return doubleTheHeroes;
+    },
 };
--- user-interface.ts
+++ user-interface.ts
@@ -1,3 +1,4 @@
+import type { HeroCard } from './api.js';
 import { game } from './game.js';
 
 const $$ = document.querySelectorAll.bind(document);
@@ -10,94 +11,111 @@ if (!conTainer) throw new Error(".cards-container not found");
 if (!btnStart) throw new Error(".btn-start not found");
 if (!countdownBar) throw new Error(".countdown-bar not found");
 
-    export function handleEvents() {
-        let the1: HTMLDivElement | null = null;
-        let the2: HTMLDivElement | null = null;
-        let timerID: number;
-        let matchedCards = 0;
-        let totalPairs = game.heroes_Data_In_Use.length;
+function renderCards(cards: HeroCard[]) {
+    const htmls = cards.map((hero) => {
+        return `
+                <div class="cards">
+                    <img src="./assets/cover.jpg" class="cover">
+                    <img src="${hero.image}" class="hero">
+                </div>
+            `
+    })
+    conTainer!.innerHTML = `<div class="row"> ${htmls.join('')} </div>`;
 
-        function setupCardConditions() {
-            const cards = document.getElementsByClassName("cards")
-            for (let card of cards) {
-                card.addEventListener("click", function (this: HTMLDivElement) {
-                    if (this === the1 || the1 !== null && the2 !== null) {
-                        return;
-                    }
-                    this.classList.add('flipped-card')
-                    if (the1 === null) {
-                        the1 = this;
-                    }
-                    else {
-                        the2 = this;
-                        checkCardStatus(the1, the2);
-                        setTimeout(function () {
-                            the1 = null;
-                            the2 = null;
-                        }, 500)
-                    }
-                })
-            }
-        }
+};
 
-        function checkCardStatus(img1: HTMLDivElement, img2: HTMLDivElement) {
-            let heroImg1 = img1.querySelector('.hero')?.getAttribute('src')
-            let heroImg2 = img2.querySelector('.hero')?.getAttribute('src')
-            // match card 
-            if (heroImg1 === heroImg2) {
-                matchedCards++
-                setTimeout(() => {
-                    img1.classList.add('match-card')
-                    img2.classList.add('match-card')
-                }, 600)
-                //THẮNG  
-                if (matchedCards == totalPairs) {
-                    clearInterval(timerID)
-                    btnStart!.classList.add('is-blinking');
-                    btnStart!.disabled = false
-                    setTimeout(() => {
-                        alert('Winner!')
+export function handleEvents() {
+    let the1: HTMLDivElement | null = null;
+    let the2: HTMLDivElement | null = null;
+    let timerID: number;
+    let matchedCards = 0;
+    let totalPairs = game.heroes_Data_In_Use.length;
+
+    function setupCardConditions() {
+        const cards = document.getElementsByClassName("cards")
+        for (let card of cards) {
+            card.addEventListener("click", function (this: HTMLDivElement) {
+                if (this === the1 || the1 !== null && the2 !== null) {
+                    return;
+                }
+                this.classList.add('flipped-card')
+                if (the1 === null) {
+                    the1 = this;
+                }
+                else {
+                    the2 = this;
+                    checkCardStatus(the1, the2);
+                    setTimeout(function () {
+                        the1 = null;
+                        the2 = null;
                     }, 500)
                 }
-            }
-            else {
-                setTimeout(function () {
-                    img1.classList.remove('flipped-card');
-                    img2.classList.remove('flipped-card');
-                }, 800);
+            })
+        }
+    }
+
+    function checkCardStatus(img1: HTMLDivElement, img2: HTMLDivElement) {
+        let heroImg1 = img1.querySelector('.hero')?.getAttribute('src')
+        let heroImg2 = img2.querySelector('.hero')?.getAttribute('src')
+        // match card 
+        if (heroImg1 === heroImg2) {
+            matchedCards++
+            setTimeout(() => {
+                img1.classList.add('match-card')
+                img2.classList.add('match-card')
+            }, 600)
+            //THẮNG  
+            if (matchedCards == totalPairs) {
+                clearInterval(timerID)
+                btnStart.classList.add('is-blinking');
+                btnStart.disabled = false
+                setTimeout(() => {
+                    alert('Winner!')
+                }, 500)
             }
         }
-        //
-        btnStart!.addEventListener("click", function () {
-            btnStart!.classList.remove('is-blinking')
-            btnStart!.disabled = true
-            conTainer!.classList.add('is-playing');
-
-            let timeLeft = 60;
-            matchedCards = 0;
-            clearInterval(timerID);
-
-            game.render();
-            setupCardConditions();
-
-            timerID = setInterval(() => {
-                timeLeft--;
-                countdownBar!.style.width = (timeLeft / 60) * 100 + '%';
-                //THUA  
-                if (timeLeft <= 0) {
-                    clearInterval(timerID);
-                    const allCards = $$('.cards');
-                    allCards.forEach((card) => {
-                        card.classList.add('is-locked')
-                    })
-                    btnStart!.classList.add('is-blinking');
-                    btnStart!.disabled = false;
-                    setTimeout(() => {
-                        alert('Thua rồi!');
-                        conTainer!.classList.add('transit-animation');
-                        conTainer!.classList.remove('is-playing');
-                    }, 500);
-                }
-            }, 1000);
-        });
+        else {
+            setTimeout(function () {
+                img1.classList.remove('flipped-card');
+                img2.classList.remove('flipped-card');
+            }, 800);
+        }
     }
+    //
+    btnStart.addEventListener("click", function () {
+        btnStart.classList.remove('is-blinking')
+        btnStart.disabled = true
+        conTainer.classList.add('is-playing');
+
+        matchedCards = 0;
+        clearInterval(timerID);
+        
+        const cards = game.createDeck();
+        renderCards(cards);
+        setupCardConditions();
+        
+        let timeLeft = 60;
+        timerID = setInterval(() => {
+            timeLeft--;
+            countdownBar.style.width = (timeLeft / 60) * 100 + '%';
+            //THUA  
+            if (timeLeft <= 0) {
+                clearInterval(timerID);
+
+                const allCards = $$('.cards');
+                allCards.forEach((card) => {
+                    card.classList.add('is-locked')
+                })
+
+                btnStart.classList.add('is-blinking');
+                btnStart.disabled = false;
+
+                setTimeout(() => {
+                    alert('Thua rồi!');
+                    conTainer.classList.add('transit-animation');
+                    conTainer.classList.remove('is-playing');
+                }, 500);
+            }
+        }, 1000);
+    });
+}
